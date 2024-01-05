f2910f93dac4b940d70b51283f85f1ed8b8106dc
Tom Tarpey <tomtarpeydev@gmail.com>
2024-01-05 22:33:07 +0000

    update signup form

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
 import './App.css';
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Dashboard from './components/Dashboard';
 import SignInForm from "./components/SignInForm";
 import SignUpForm from "./components/SignUpForm";
 
 function App() {
 
+
   return (
    <div>
       <h1 className="text-3xl font-bold underline">Lets get Started!!!</h1>
       <Router> 
         <Routes>
-        <Route exact path="/" element={<Dashboard />} />
-        <Route  path="/sign-in" element={<SignInForm />} />
-        <Route  path="/sign-up" element={<SignUpForm />} />
-
-          </Routes>   
+          <Route exact path="/" element={<Dashboard />} />
+          <Route  path="/sign-in" element={<SignInForm />} />
+          <Route  path="/sign-up" element={<SignUpForm />} />
+        </Routes>   
       </Router>
    </div>
   )
--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,9 +1,33 @@
 const SignUpForm = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+  
+    const handleSignup = async (e) => {
+      e.preventDefault();
+  
+      try {
+        await axios.post('API_SIGNUP_ENDPOINT', {
+          email,
+          password,
+        });
+  
+        history.push('/login');
+      } catch (error) {
+        console.error('Signup failed', error);
+      }
+    };
+  
     return (
-        <div>
-            <h2>Signup Form</h2>
-        </div>
-    );
+      <form onSubmit={handleSignup}>
+        <label>Email:</label>
+        <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+  
+        <label>Password:</label>
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+  
+        <button type="submit">Signup</button>
+      </form>
+    )
 }
 
 export default SignUpForm;
